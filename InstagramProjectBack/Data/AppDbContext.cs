70e126caa4f1a2fddd3e7289c9d39621e8dcf4d2
SANI024 <sandropitiurishvili0@gmail.com>
2025-05-01 18:22:32 +0400

    just back code

---
+++ InstagramProjectBack/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace InstagramProjectBack.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
